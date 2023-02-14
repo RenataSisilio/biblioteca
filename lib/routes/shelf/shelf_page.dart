56481145071586ab4e242ec651ef63f71140630c
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2023-02-14 13:39:37 -0300

    feat: shelf view

--- lib/routes/shelf/shelf_page.dart
+++ lib/routes/shelf/shelf_page.dart
@@ -1,39 +1,30 @@
+import 'package:biblioteca/routes/shelf/shelf_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../models/book.dart';
-import '../../widgets/book_info.dart';
+import '../../get_it.dart';
+import '../../services/controllers/library_controller.dart';
 
-class ShelfView extends StatelessWidget {
-  const ShelfView({
-    super.key,
-    required this.books,
-    required this.category,
-  });
+class ShelfPage extends StatelessWidget {
+  const ShelfPage(this.category, {super.key});
 
-  final List<Book> books;
   final String category;
 
   @override
   Widget build(BuildContext context) {
-    final shelf = books.where((book) => book.category == category).toList();
-    final copy = [...shelf];
-    for (var i = 1; i < copy.length; i++) {
-      if (copy[i].title == copy[i - 1].title) {
-        copy.remove(copy[i]);
-        i--;
-      }
-    }
     return Scaffold(
       appBar: AppBar(title: Text(category)),
-      body: ListView.builder(
-        itemCount: copy.length,
-        itemBuilder: (context, index) => ExpansionTile(
-          title: Text(copy[index].title),
-          subtitle: Text(copy[index].author ?? ''),
-          children: List.from(shelf
-              .where((e) => e.title == copy[index].title)
-              .map((e) => BookInfo(books.indexOf(e)))),
-        ),
+      body: BlocBuilder<LibraryController, LibraryState>(
+        bloc: getIt.get<LibraryController>(),
+        builder: (context, state) {
+          if (state == LibraryState.loading) {
+          return const Center(child: CircularProgressIndicator());
+          }
+          if (state == LibraryState.error) {
+            return const Center(child: Text('Erro'));
+          }
+            return ShelfView(category);
+        },
       ),
     );
   }
