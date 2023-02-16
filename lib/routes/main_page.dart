5c1da7529f83461355833e65be679b660c74103c
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2023-02-16 11:23:03 -0300

    feat: connection controller

--- lib/routes/main_page.dart
+++ lib/routes/main_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../widgets/bottom_bar.dart';
+import '../get_it.dart';
+import '../services/controllers/connection_controller.dart';
 import 'home/home_page.dart';
 import 'library/library_page.dart';
 
@@ -15,10 +18,24 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     final pageController = PageController();
+    final connectionController = getIt.get<ConnectionController>();
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Biblioteca Contemplativa'),
+        actions: [
+          BlocBuilder<ConnectionController, ConnectState>(
+            bloc: connectionController,
+            builder: (context, state) {
+              return IconButton(
+                onPressed: () => connectionController.checkConnection(),
+                icon: Icon(state == ConnectState.online
+                    ? Icons.sync
+                    : Icons.sync_problem),
+              );
+            },
+          )
+        ],
       ),
       body: PageView(
         controller: pageController,
--- lib/services/controllers/connection_controller.dart
+++ lib/services/controllers/connection_controller.dart
@@ -1,1 +1,28 @@
-class ConnectionController{}
+import 'package:bloc/bloc.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+enum ConnectState { online, offline }
+
+class ConnectionController extends Cubit<ConnectState> {
+  ConnectionController() : super(ConnectState.offline) {
+    checkConnection();
+    init();
+  }
+
+  void init() async {
+    while (true) {
+      await Future.delayed(const Duration(minutes: 5));
+      checkConnection();
+    }
+  }
+
+  Future<void> checkConnection() async {
+    final connection = await Connectivity().checkConnectivity();
+    if (connection != ConnectivityResult.none &&
+        connection != ConnectivityResult.bluetooth) {
+      emit(ConnectState.online);
+    } else {
+      emit(ConnectState.offline);
+    }
+  }
+}
