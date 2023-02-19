acea89760d00517043c43930c6370419e6bf7ce7
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2023-02-19 13:10:41 -0300

    style: remove unused widgets and imports

--- lib/routes/main_page.dart
+++ lib/routes/main_page.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../widgets/bottom_bar.dart';
-import '../get_it.dart';
-import '../services/controllers/connection_controller.dart';
 import 'home/home_page.dart';
 import 'library/library_page.dart';
 
@@ -18,37 +15,16 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     final pageController = PageController();
-    final connectionController = getIt.get<ConnectionController>();
 
     return Scaffold(
       appBar: PreferredSize(
         preferredSize:
             Size(double.infinity, MediaQuery.of(context).size.height * 0.15),
-        child: Row(
-          children: [
-            Expanded(
-              child: Center(
-                child: Image.asset(
-                  'assets/Minha Biblioteca Contemplativa.png',
-                  height: MediaQuery.of(context).size.height * 0.15,
-                ),
-              ),
-            ),
-            // Padding(
-            //   padding: const EdgeInsets.only(right: 8.0),
-            //   child: BlocBuilder<ConnectionController, ConnectState>(
-            //     bloc: connectionController,
-            //     builder: (context, state) {
-            //       return IconButton(
-            //         onPressed: () => connectionController.checkConnection(),
-            //         icon: Icon(state == ConnectState.online
-            //             ? Icons.sync
-            //             : Icons.sync_problem),
-            //       );
-            //     },
-            //   ),
-            // ),
-          ],
+        child: Center(
+          child: Image.asset(
+            'assets/Minha Biblioteca Contemplativa.png',
+            height: MediaQuery.of(context).size.height * 0.15,
+          ),
         ),
       ),
       body: PageView(
